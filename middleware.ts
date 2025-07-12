5f640ea87e259f31a1e99baed8fe6bc88623e634
Swati Sharma <scarletshukurenai@gmail.com>
2025-07-12 10:55:29 +0530

    backend: clerk and mongo setup

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/question/(.*)',
+  '/search',
+  '/api/questions(.*)',
+  '/api/search',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+])
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    // auth().protect() - removed this line
+  }
+})
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+}
